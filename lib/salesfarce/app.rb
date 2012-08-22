7044d1b0b8053498da948e5eff17e54b35d1a0e4
Ryan Houston <ryanhouston83@gmail.com>
2012-08-22 16:19:01 -0400

    Add a layout and handle auth checks on all requests

--- lib/salesfarce/app.rb
+++ lib/salesfarce/app.rb
@@ -21,16 +21,24 @@ module Salesfarce
       exception = env['sinatra.error']
       if exception.error_code == "INVALID_SESSION_ID"
         session[:client] = nil
-        @notice = "Your session expired and you were logged out!"
+        @flash[:notice] = "Your session expired and you were logged out!"
       else
-        @notice = "#{exception.error_code}: #{exception.message}"
+        @flash[:notice] = "#{exception.error_code}: #{exception.message}"
       end
-      redirect to("/")
+    end
+
+    before do
+      @authenticated = true
+      @flash = {}
+
+      unless (session[:client])
+        @flash[:notice] = 'You are not authenticated'
+        @authenticated = false
+        redirect to('/') unless (request.path_info == '/' || /\/auth\/salesforce/.match(request.path_info))
+      end
     end
 
     get '/' do
-      puts session.inspect
-
       haml :home
     end
 
@@ -44,6 +52,11 @@ module Salesfarce
       session[:client] = Databasedotcom::Client.new("config/salesforce.yml")
       session[:client].authenticate request.env['omniauth.auth']
       redirect to("/")
+    end
+
+    get '/logout' do
+      session[:client] = nil
+      redirect to('/')
     end
 
     def self.start
