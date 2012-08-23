d9699097d7936d7c5b69a186e9e370c395094622
Ryan Houston <ryanhouston83@gmail.com>
2012-08-23 16:09:13 -0400

    Add placeholder links for whats next

--- lib/salesfarce/user.rb
+++ lib/salesfarce/user.rb
@@ -2,6 +2,7 @@ class User
   include DataMapper::Resource
 
   property :id,             Serial  # auto-increment integer PK
+  property :username,       String, :required => true
   property :first_name,     String
   property :last_name,      String, :required => true
   property :company,        String
